1c417ddc7a7e6f13fb79950ed61934fc0c5f2e2f
guptarpi <guptarpi@deshaw.com>
2022-07-02 19:43:20 +0530

    test

---
+++ Practice.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Practice {
+
+    public int sum(List<Integer> a) {
+        Integer res = 0;
+        for (Integer i : a) {
+            res += i;
+        }
+        return res;
+    }
+}
